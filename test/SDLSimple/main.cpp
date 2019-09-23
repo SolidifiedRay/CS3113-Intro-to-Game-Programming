7d987be82768c748c79cce4c1e974c81eb4e5d75
SolidifiedRay <ray2147483647@outlook.com>
2019-09-23 11:47:27 -0400

    in-class practise

--- test/SDLSimple/main.cpp
+++ test/SDLSimple/main.cpp
@@ -6,9 +6,13 @@
 
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <SDL_image.h>
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
@@ -16,9 +20,35 @@ bool gameIsRunning = true;
 ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
 
+glm::vec3 player_position = glm::vec3(0, 0, 0);
+glm::vec3 player_movement = glm::vec3(0, 0, 0);
+
+GLuint playerTextureID;
+
+GLuint LoadTexture(const char* filePath) {
+	int w, h, n;
+	unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+	if (image == NULL) {
+		std::cout << "Unable to load image. Make sure the path is correct\n";
+		assert(false);
+	}
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_image_free(image);
+	return textureID;
+}
+
 void Initialize() {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("Triangle!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("Textured", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -28,7 +58,9 @@ void Initialize() {
 
 	glViewport(0, 0, 640, 480);
 
-	program.Load("shaders/vertex.glsl", "shaders/fragment.glsl");
+	program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+
+	playerTextureID = LoadTexture("me.png");
 
 	viewMatrix = glm::mat4(1.0f);
 	modelMatrix = glm::mat4(1.0f);
@@ -36,34 +68,100 @@ void Initialize() {
 
 	program.SetProjectionMatrix(projectionMatrix);
 	program.SetViewMatrix(viewMatrix);
-	program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+	program.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glUseProgram(program.programID);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 }
 
 void ProcessInput() {
+	player_movement = glm::vec3(0, 0, 0);
+
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+		switch (event.type) {
+		case SDL_QUIT:
+		case SDL_WINDOWEVENT_CLOSE:
 			gameIsRunning = false;
+			break;
+
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym) {
+			case SDLK_SPACE:
+				// Some sort of action
+				break;
+
+			}
+			break;
 		}
+	}
+
+	// Check for pressed/held keys below
+	const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+	if (keys[SDL_SCANCODE_A])
+	{
+		player_movement.x = -1;
+	}
+	else if (keys[SDL_SCANCODE_D])
+	{
+		player_movement.x = 1;
+	}
+
+	if (keys[SDL_SCANCODE_W])
+	{
+		player_movement.y = 1;
+	}
+	else if (keys[SDL_SCANCODE_S])
+	{
+		player_movement.y = -1;
+	}
+
+	if (glm::length(player_movement) > 1.0f)
+	{
+		player_movement = glm::normalize(player_movement);
 	}
 }
 
-void Update() { }
+float lastTicks = 0;
+
+void Update() {
+	float ticks = (float)SDL_GetTicks() / 1000.0f;
+	float deltaTime = ticks - lastTicks;
+	lastTicks = ticks;
+
+	player_position += player_movement * deltaTime;
+
+
+	modelMatrix = glm::mat4(1.0f);
+	modelMatrix = glm::translate(modelMatrix, player_position);
+}
 
 void Render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	program.SetModelMatrix(modelMatrix);
 
-	float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
+	float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+	float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
+	glBindTexture(GL_TEXTURE_2D, playerTextureID);
+
 	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
 	glEnableVertexAttribArray(program.positionAttribute);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
 	glDisableVertexAttribArray(program.positionAttribute);
+	glDisableVertexAttribArray(program.texCoordAttribute);
 
 	SDL_GL_SwapWindow(displayWindow);
 }
