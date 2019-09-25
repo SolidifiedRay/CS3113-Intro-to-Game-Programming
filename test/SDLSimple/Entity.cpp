0d517a44560a5fef15888323514bc41df64efc97
SolidifiedRay <ray2147483647@outlook.com>
2019-09-25 11:41:04 -0400

    test Entity example

---
+++ test/SDLSimple/Entity.cpp
@@ -0,0 +1,34 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+	position = glm::vec3(0);
+	speed = 0;
+}
+
+void Entity::Update(float deltaTime)
+{
+	position += movement * speed * deltaTime;
+}
+
+void Entity::Render(ShaderProgram *program) {
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	modelMatrix = glm::translate(modelMatrix, position);
+	program->SetModelMatrix(modelMatrix);
+
+	float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+	float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+	glEnableVertexAttribArray(program->positionAttribute);
+
+	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+	glEnableVertexAttribArray(program->texCoordAttribute);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glDisableVertexAttribArray(program->positionAttribute);
+	glDisableVertexAttribArray(program->texCoordAttribute);
+}
---
+++ test/SDLSimple/Entity.h
@@ -0,0 +1,29 @@
+#pragma once
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+
+class Entity {
+public:
+
+	glm::vec3 position;
+	glm::vec3 movement;
+	float speed;
+
+	GLuint textureID;
+
+	Entity();
+
+	void Update(float deltaTime);
+	void Render(ShaderProgram *program);
+};
