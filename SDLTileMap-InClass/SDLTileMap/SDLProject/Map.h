d9da5c16e16b91d0caf4b6f516b08973eeb24e59
SolidifiedRay <ray2147483647@outlook.com>
2019-10-28 11:47:46 -0400

    In Class Practise Map

---
+++ SDLTileMap-InClass/SDLTileMap/SDLProject/Map.h
@@ -0,0 +1,23 @@
+#pragma once
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+
+class Map {
+public:
+        
+    
+};
+
+
+
---
+++ in-class-test3/SDLSimple/main.cpp
@@ -0,0 +1,181 @@
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <vector>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "Entity.h"
+#include "Map.h"
+
+SDL_Window* displayWindow;
+bool gameIsRunning = true;
+
+ShaderProgram program;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+#define ENEMY_COUNT 1
+
+struct GameState {
+	Entity player;
+	Entity enemies[ENEMY_COUNT];
+	Map *map;
+};
+
+GameState state;
+
+GLuint LoadTexture(const char* filePath) {
+	int w, h, n;
+	unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+	if (image == NULL) {
+		std::cout << "Unable to load image. Make sure the path is correct\n";
+		assert(false);
+	}
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_image_free(image);
+	return textureID;
+}
+
+void Initialize() {
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("Platformer!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+
+#ifdef _WINDOWS
+	glewInit();
+#endif
+
+	glViewport(0, 0, 640, 480);
+
+	program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+
+	state.player.entityType = PLAYER;
+	state.player.isStatic = false;
+	state.player.width = 1.0f;
+	state.player.position = glm::vec3(0, 3, 0);
+	state.player.acceleration = glm::vec3(0, -9.81f, 0);
+	state.player.textureID = LoadTexture("me.png");
+
+
+	viewMatrix = glm::mat4(1.0f);
+	modelMatrix = glm::mat4(1.0f);
+	projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+	program.SetProjectionMatrix(projectionMatrix);
+	program.SetViewMatrix(viewMatrix);
+	program.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glUseProgram(program.programID);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+}
+
+void ProcessInput() {
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+		case SDL_WINDOWEVENT_CLOSE:
+			gameIsRunning = false;
+			break;
+
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym) {
+			case SDLK_SPACE:
+				state.player.Jump();
+				break;
+
+			}
+			break;
+		}
+	}
+
+	state.player.velocity.x = 0;
+
+	// Check for pressed/held keys below
+	const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+	if (keys[SDL_SCANCODE_A])
+	{
+		state.player.velocity.x = -3.0f;
+	}
+	else if (keys[SDL_SCANCODE_D])
+	{
+		state.player.velocity.x = 3.0f;
+	}
+}
+
+#define FIXED_TIMESTEP 0.0166666f
+float lastTicks = 0;
+float accumulator = 0.0f;
+
+void Update() {
+	float ticks = (float)SDL_GetTicks() / 1000.0f;
+	float deltaTime = ticks - lastTicks;
+	lastTicks = ticks;
+
+	deltaTime += accumulator;
+	if (deltaTime < FIXED_TIMESTEP) {
+		accumulator = deltaTime;
+		return;
+	}
+
+	while (deltaTime >= FIXED_TIMESTEP) {
+
+		state.player.Update(FIXED_TIMESTEP, state.enemies, ENEMY_COUNT, state.map);
+
+		deltaTime -= FIXED_TIMESTEP;
+	}
+
+	accumulator = deltaTime;
+}
+
+
+void Render() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	state.player.Render(&program);
+
+	SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+	SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+	Initialize();
+
+	while (gameIsRunning) {
+		ProcessInput();
+		Update();
+		Render();
+	}
+
+	Shutdown();
+	return 0;
+}
